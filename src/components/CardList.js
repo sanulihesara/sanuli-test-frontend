71d13337ae23c060a54ac286b57226bb36d7609b
Sanuli <sanulicooray84@gmail.com>
2025-02-12 14:22:28 +0530

    Added a new skill

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,7 +5,7 @@ const CardList = ({ showCards }) => {
   if (!showCards) return null;
 
   const cards = [
-    { title: "Skill 1", description: "Description of Skill 1" },
+    { title: "Intrested in playing guitar", description: "I've been playing guitar for about three years now and have improved my skills. I play both rhythm and lead guitar and have also performed in a mini concert with my friends in our music class. Our teacher is very supportive and always helps us; because of that, our love for music never fades." },
     { title: "Skill 2", description: "Description of Skill 2" },
   ];
 
